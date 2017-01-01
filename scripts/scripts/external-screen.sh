2d6da3298040d247ce6389ff2737b359bc1590ed
Barry Gackle <bdgackle@gmail.com>
2017-01-01 14:40:51 -0800

    Automate external display connection

---
+++ scripts/scripts/external-screen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --output HDMI1 --above eDP1 --auto
+~/.fehbg
+xrdb ~/.Xresources-1080p
+xset s off
+xset -dpms
---
+++ scripts/scripts/no-external-screen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --output HDMI1 --off
+~/.fehbg
+xrdb ~/.Xresources
+xset s on
+xset dpms
