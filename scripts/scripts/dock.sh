0cd7cd47a58d02b950c8894ee4c35213ae4d7197
Barry Gackle <bdgackle@gmail.com>
2017-02-18 18:17:34 -0800

    Rename dock/undock scripts

--- scripts/scripts/dock.sh
+++ scripts/scripts/dock.sh
@@ -5,3 +5,5 @@ xrandr --output HDMI1 --above eDP1 --auto
 xrdb ~/.Xresources-1080p
 xset s off
 xset -dpms
+sudo systemctl restart dhcpcd
+sudo netctl stop-all
--- scripts/scripts/undock.sh
+++ scripts/scripts/undock.sh
@@ -5,3 +5,6 @@ xrandr --output HDMI1 --off
 xrdb ~/.Xresources
 xset s on
 xset dpms
+sudo ip link set wlp2s0 down
+sudo netctl stop-all
+sudo netctl start wlp2s0-Synapse
